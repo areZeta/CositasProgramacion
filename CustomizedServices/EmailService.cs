55d0a6175612620f62274172009035b47746fef8
DEVAPPSEC\appse <appsec2023@outlook.com>
2023-04-16 20:35:12 -0600

    EmailService

---
+++ CustomizedServices/EmailService.cs
@@ -0,0 +1,64 @@
+﻿using System.Net.Mail;
+using System.Net;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace CustomizedServices
+{
+    public class EmailService
+    {
+        private readonly IConfiguration _config;
+
+        public EmailService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public (int StatusCode, string Message, MailMessage email) ConfigurationEmailAsync(string to, string subject, string body)
+        {
+            MailMessage? email = new MailMessage();
+            try
+            {
+                if (to == "" || subject == "" || body == "") return (StatusCodes.Status400BadRequest, "Require some data", email);
+                var from = _config.GetSection("Smtp:From").Value;
+                var host = _config.GetSection("Smtp:Host").Value;
+                var port = _config.GetSection("Smtp:Port").Value;
+                var password = _config.GetSection("Smtp:Password").Value;
+                var displayName = _config.GetSection("Smtp:UserName").Value; ;
+
+                email.From = new MailAddress(from, displayName);//Correo de salida
+                email.To.Add(to); //Correo destino?
+                email.Subject = subject; //Asunto
+                email.Body = body; //Mensaje del correo
+                email.IsBodyHtml = true;
+                email.Priority = MailPriority.High;
+                return (StatusCodes.Status200OK, "Successful", email);
+            }
+            catch (Exception ex)
+            {
+                return (StatusCodes.Status500InternalServerError, ex.Message, email);
+            }
+        }
+
+        public (int StatusCode, string Message) SendEmailAsync(MailMessage email)
+        {
+            try
+            {
+                var from = _config.GetSection("Smtp:From").Value;
+                var host = _config.GetSection("Smtp:Host").Value;
+                var port = _config.GetSection("Smtp:Port").Value;
+                var password = _config.GetSection("Smtp:Password").Value;
+
+                SmtpClient client = new SmtpClient(host, int.Parse(port));
+                client.Credentials = new NetworkCredential(from, password);//Cuenta de correo
+                client.EnableSsl = true;//True si el servidor de correo permite ssl
+                client.Send(email);
+                return (StatusCodes.Status200OK, "Succesful send email");
+            }
+            catch (Exception ex)
+            {
+                return (StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
---
+++ CustomizedServices/UrlService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CustomizedServices
+{
+    public class UrlService
+    {
+        public UrlService() { }
+        public string ChangeUrlParamsToPath(string Url)
+        {
+            string pattern = @"\?\w*=|\&\w*=";
+            string replacement = "/";
+            Url = Regex.Replace(Url, pattern, replacement);
+            return Url;
+        }
+    }
+}
